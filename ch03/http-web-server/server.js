7be728d2e3824f94ef5ab7ee0aeacb27074b392c
Dan Carlier <dan.carlier@gmail.com>
2022-10-17 11:36:48 +1100

    Node core server with two routes

--- ch03/http-web-server/server.js
+++ ch03/http-web-server/server.js
@@ -1,6 +1,8 @@
 'use strict'
+const url = require('url')
 const http = require('http')
 const PORT = process.env.PORT || 3000
+const { STATUS_CODES } = http
 
 const hello = `<html>
   <head>
@@ -14,9 +16,36 @@ const hello = `<html>
   </body>
 </html>`
 
+const root = `<html>
+  <head>
+    <style>
+      body { background: #333; margin: 1.25rem }
+      a { color: #EEE; font-size: 2rem; font-family: sans-serif }
+    </style>
+  </head>
+  <body>
+    <a href='/hello'>Hello</a>
+  </body>
+</html>`
+
 const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html')
-  res.end(hello)
+  if (req.method !== 'GET') {
+    res.statusCode = 405
+    res.end(STATUS_CODES[res.statusCode] + '\r\n')
+    return
+  }
+  const { pathname } = url.parse(req.url)
+  if (pathname === '/') {
+    res.end(root)
+    return
+  }
+  if (pathname === '/hello') {
+    res.end(hello)
+    return
+  }
+  res.statusCode = 404
+  res.end(STATUS_CODES[res.statusCode] + '\r\n')
 })
 
 server.listen(PORT)
