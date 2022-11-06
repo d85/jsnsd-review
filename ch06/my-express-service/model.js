7ff2e5e843184b0aa413154f57daf39ca670864c
Dan Carlier <dan.carlier@gmail.com>
2022-11-07 10:02:08 +1100

    Updated bicyle model with uid, create, update and delete functions

--- ch06/my-express-service/model.js
+++ ch06/my-express-service/model.js
@@ -6,6 +6,24 @@ function bicycleModel () {
     2: { brand: 'Batavus', color: 'yellow' }
   }
 
+  function uid () {
+    return Object.keys(db)
+      .sort((a, b) => a - b)
+      .map(Number)
+      .filter((n) => !isNaN(n))
+      .pop() + 1 + ''
+  }
+
+  function create (id, data, cb) {
+    if (db.hasOwnProperty(id)) {
+      const err = Error('resource exists')
+      setImmediate(() => cb(err))
+      return
+    }
+    db[id] = data
+    setImmediate(() => cb(null, id))
+  }
+
   function read (id, cb) {
     if (!(db.hasOwnProperty(id))) {
       const err = Error('not found')
@@ -15,8 +33,32 @@ function bicycleModel () {
     setImmediate(() => cb(null, db[id]))
   }
 
+  function update (id, data, cb) {
+    if (!(db.hasOwnProperty(id))) {
+      const err = Error('not found')
+      setImmediate(() => cb(err))
+      return
+    }
+    db[id] = data
+    setImmediate(() => cb())
+  }
+
+  function del (id, cb) {
+    if (!(db.hasOwnProperty(id))) {
+      const err = Error('not found')
+      setImmediate(() => cb(err))
+      return
+    }
+    delete db[id]
+    setImmediate(() => cb())
+  }
+
   return {
-    read
+    uid,
+    create,
+    read,
+    update,
+    del
   }
 }
 
