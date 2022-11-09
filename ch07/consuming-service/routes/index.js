e73d4f9ae7b00be341be0c06b7501cf83d6f338d
Dan Carlier <dan.carlier@gmail.com>
2022-11-10 10:28:53 +1100

    Call bicycle endpoint with id and return the result

--- ch07/consuming-service/routes/index.js
+++ ch07/consuming-service/routes/index.js
@@ -1,8 +1,19 @@
-var express = require('express');
-var router = express.Router();
+'use strict'
+var express = require('express')
+var router = express.Router()
+const got = require('got')
 
-router.get('/', function(req, res, next) {
-  res.send({ root: true });
+const {
+  BICYCLE_SERVICE_PORT = 4000
+} = process.env
+
+const bicycleSrv = `http://localhost:${BICYCLE_SERVICE_PORT}`
+
+router.get('/:id', function (req, res, next) {
+  const { id } = req.params
+
+  got(`${bicycleSrv}/${id}`).json()
+    .then(bicycle => res.send(bicycle))
 });
 
-module.exports = router;
+module.exports = router
