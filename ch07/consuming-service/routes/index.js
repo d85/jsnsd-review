4068cfd2e6d24e57910a7b41c193e9cd84b46431
Dan Carlier <dan.carlier@gmail.com>
2022-11-10 10:42:32 +1100

    Call bicycle and brand service and retur the combined result

--- ch07/consuming-service/routes/index.js
+++ ch07/consuming-service/routes/index.js
@@ -4,16 +4,26 @@ var router = express.Router()
 const got = require('got')
 
 const {
-  BICYCLE_SERVICE_PORT = 4000
+  BICYCLE_SERVICE_PORT = 4000, BRAND_SERVICE_PORT = 5000
 } = process.env
 
 const bicycleSrv = `http://localhost:${BICYCLE_SERVICE_PORT}`
+const brandSrv = `http://localhost:${BRAND_SERVICE_PORT}`
 
 router.get('/:id', function (req, res, next) {
   const { id } = req.params
 
-  got(`${bicycleSrv}/${id}`).json()
-    .then(bicycle => res.send(bicycle))
+  const bicylePromise = got(`${bicycleSrv}/${id}`).json()
+  const brandPromise = got(`${brandSrv}/${id}`).json()
+
+  Promise.all([bicylePromise, brandPromise])
+    .then(([ bicycle, brand ]) => {
+      res.send({
+        id: bicycle.id,
+        color: bicycle.color,
+        brand: brand.name,
+      })
+    })
 });
 
 module.exports = router
