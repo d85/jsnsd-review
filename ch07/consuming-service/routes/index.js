1b9f7d4a5eaa8c5ca63224ec00620d944d6039f8
Dan Carlier <dan.carlier@gmail.com>
2022-11-18 07:45:41 +1100

    ch-07 handle error scenarios

--- ch07/consuming-service/routes/index.js
+++ ch07/consuming-service/routes/index.js
@@ -1,4 +1,5 @@
 'use strict'
+var createError = require('http-errors');
 var express = require('express')
 var router = express.Router()
 const got = require('got')
@@ -13,17 +14,23 @@ const brandSrv = `http://localhost:${BRAND_SERVICE_PORT}`
 router.get('/:id', function (req, res, next) {
   const { id } = req.params
 
-  const bicylePromise = got(`${bicycleSrv}/${id}`).json()
-  const brandPromise = got(`${brandSrv}/${id}`).json()
-
-  Promise.all([bicylePromise, brandPromise])
-    .then(([ bicycle, brand ]) => {
-      res.send({
-        id: bicycle.id,
-        color: bicycle.color,
-        brand: brand.name,
-      })
+  Promise.all([
+    got(`${bicycleSrv}/${id}`).json(), 
+    got(`${brandSrv}/${id}`).json()
+  ])
+  .then(([ bicycle, brand ]) => {
+    res.send({
+      id: bicycle.id,
+      color: bicycle.color,
+      brand: brand.name,
     })
+  })
+  .catch((err) => {
+    if (!err.response) next(err)
+    if (err.response.statusCode === 404) next()
+    if (err.response.statusCode === 400) next(createError(400))
+    else next(err)
+  })
 });
 
 module.exports = router
